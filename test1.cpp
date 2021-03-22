fac709f850505bb508f9af3283322314029a48a8
ebenemfum <outbreak@LAPTOP-CB7HJSO1.localdomain>
2021-03-22 01:34:28 +0000

    Test file

---
+++ test1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	int a, b = 10, c;
+	c = 11;
+
+	cout << b << c << a << endl;
+	cout << "Hello World!" << endl;
+
+	cout << "First CODE!";
+
+	return 0;
+}
