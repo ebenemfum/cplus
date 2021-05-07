74861e14475ad515fc538fd63767d3a79c6d410f
ebenemfum <ebenezer.amoah@ashesi.edu.gh>
2021-05-07 20:56:17 +0000

    Final LAB

---
+++ LaboratoryExercise11(Project)/rectBlock.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#define MAXARRAYSIZE 500;
+using namespace std;
+
+class rectBlocks // The rectBlocks class
+{
+public:         // Access specifier
+    int width;  // Width of Rectangle
+    int height; // Height of Rectangle
+    int length; // Length of Rectangle
+};
+
+vector<rectBlocks> rectangleBlocks;
+
+int main()
+{
+
+    int w, h, l;
+
+    int count = 0;
+
+    char comma;
+
+    int counts = 0;
+
+    ifstream input("dataBlocks.dat");
+
+    while (input >> w >> comma >> h >> comma >> l)
+    {
+        rectBlocks RBlock;
+
+        RBlock.width = w;
+
+        RBlock.height = h;
+
+        RBlock.length = l;
+
+        cout << RBlock.length << endl;
+
+        rectangleBlocks.push_back(RBlock);
+
+        ++counts;
+    }
+};
